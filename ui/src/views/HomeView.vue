018ee823e17ccd328a4f950926022eff8e9b29ab
Serban Andrei <usadekall@gmail.com>
2022-12-10 17:15:55 +0200

    Map added

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,13 +1,21 @@
 <template>
-  <v-app-bar elevation="4" color="#ACD1E8">
-    <v-icon class="mr-4">fa-backward</v-icon>
-    <v-app-title>{{title}}</v-app-title>
-  </v-app-bar>
+  <div>
+    <v-app-bar elevation="4" color="#ACD1E8">
+      <v-icon class="mr-4">fa-backward</v-icon>
+      <v-app-title>{{title}}</v-app-title>
+    </v-app-bar>
+    <MapComp />
+  </div>
 </template>
 
 <script>
+  import MapComp from "../components/Map.vue";
+
   export default {
     name: 'HomeView',
+    components: {
+      MapComp
+    },
     data: () => ({
       title: 'Programare vizita sucursala',
     }),
