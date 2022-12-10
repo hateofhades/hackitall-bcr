9e45ef27422f05792d2e1f87a8c21db37f496b54
Grigore Irina <irinagrigore11@gmail.com>
2022-12-10 16:29:11 +0200

    added progress bar

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -16,3 +16,9 @@ export default {
   }),
 };
 </script>
+
+<style>
+  body {
+    margin: 0
+  }
+</style>
--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,28 +1,89 @@
 <template>
+  <v-container class="pa-0 ma-0" fluid>
+    <v-app-bar color="#AAE1FA" rounded>
+      <v-btn icon fab dark small color="primary">
+        <v-icon class="mx-2" dark left>mdi-arrow-left</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-toolbar-title class="font-weight-bold">{{ title }}</v-toolbar-title>
+      <img
+        class="mr-3"
+        :src="
+          require('../../public/img/logo-BCR-high-resolution-1980x1080-1140x560.jpg')
+        "
+        height="40"
+      />
+      <v-spacer></v-spacer>
+    </v-app-bar>
 
-  <v-app-bar class="flex-grow-0" color="#AAE1FA" rounded >
-    <v-btn icon
-    class="mx-2"
-    fab
-    dark
-    small
-    color="primary">
-      <v-icon class="mr-4">fa-backward</v-icon>
-    </v-btn>
-    <v-spacer></v-spacer>
-    <v-toolbar-title class="font-weight-bold" >{{title}}</v-toolbar-title>
-    <img class="mr-3" :src="require('../../public/img/logo-BCR-high-resolution-1980x1080-1140x560.jpg')" height="40"/>
-    <v-spacer></v-spacer>
-  </v-app-bar>
+    <v-row justify="center">
+      <v-col cols="10">
+        <v-stepper v-model="e1" class="mt-4">
+          <v-stepper-header>
+            <v-stepper-step :complete="e1 > 1" step="1">
+              Alege scop vizita
+            </v-stepper-step>
 
+            <v-divider></v-divider>
+
+            <v-stepper-step :complete="e1 > 2" step="2">
+              Alege locatia
+            </v-stepper-step>
+
+            <v-divider></v-divider>
+
+            <v-stepper-step step="3"> Alege ziua si data </v-stepper-step>
+          </v-stepper-header>
+
+          <v-stepper-items>
+            <v-stepper-content step="1">
+              <v-card
+                class="mb-12"
+                color="grey lighten-1"
+                height="200px"
+              ></v-card>
+
+              <v-btn color="primary" @click="e1 = 2"> Continue </v-btn>
+
+              <v-btn text> Cancel </v-btn>
+            </v-stepper-content>
+
+            <v-stepper-content step="2">
+              <v-card
+                class="mb-12"
+                color="grey lighten-1"
+                height="200px"
+              ></v-card>
+
+              <v-btn color="primary" @click="e1 = 3"> Continue </v-btn>
+
+              <v-btn text> Cancel </v-btn>
+            </v-stepper-content>
+
+            <v-stepper-content step="3">
+              <v-card
+                class="mb-12"
+                color="grey lighten-1"
+                height="200px"
+              ></v-card>
+
+              <v-btn color="primary" @click="e1 = 1"> Continue </v-btn>
+
+              <v-btn text> Cancel </v-btn>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-
 <script>
-  export default {
-    name: 'HomeView',
-    data: () => ({
-      title: 'Programare vizita sucursala',
-    }),
-  }
+export default {
+  name: "HomeView",
+  data: () => ({
+    title: "Programare vizita sucursala",
+    e1: 1,
+  }),
+};
 </script>
