28c84daaa13d589e0b95f130123c49c6b4243073
Grigore Irina <irinagrigore11@gmail.com>
2022-12-10 15:42:14 +0200

    update on home view

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <template>
-  <v-app-bar elevation="4" color="#ACD1E8">
-    <v-icon class="mr-4">fa-backward</v-icon>
-    <v-app-title>{{title}}</v-app-title>
+
+  <v-app-bar class="flex-grow-0" color="#AAE1FA" rounded >
+    <v-btn icon
+    class="mx-2"
+    fab
+    dark
+    small
+    color="primary">
+      <v-icon class="mr-4">fa-backward</v-icon>
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-toolbar-title class="font-weight-bold" >{{title}}</v-toolbar-title>
+    <img class="mr-3" :src="require('../../public/img/logo-BCR-high-resolution-1980x1080-1140x560.jpg')" height="40"/>
+    <v-spacer></v-spacer>
   </v-app-bar>
+
 </template>
+
 
 <script>
   export default {
