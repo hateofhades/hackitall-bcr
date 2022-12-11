522829dec08872121405d31edabfc63b9979a317
Jumarea Stefan <69107167+StefanJum@users.noreply.github.com>
2022-12-11 03:32:16 +0200

    Done app (#27)
    * Fix date submition
    
    Signed-off-by: Stefan Jumarea <stefanjumarea02@gmail.com>
    
    * Change appointment cancellation confirmation
    
    Signed-off-by: Stefan Jumarea <stefanjumarea02@gmail.com>
    
    * Add finish appointment page
    
    Signed-off-by: Stefan Jumarea <stefanjumarea02@gmail.com>
    
    Signed-off-by: Stefan Jumarea <stefanjumarea02@gmail.com>

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -4,6 +4,8 @@ import HomeView from '../views/HomeView.vue'
 import MapComp from '../components/Map.vue'
 import TermsAndConditions from '../components/TermsAndConditions.vue'
 import CanceledAppointment from '../components/Canceled.vue'
+import DoneAppoinment from '../components/DoneAppointment.vue'
+
 
 Vue.use(VueRouter)
 
@@ -27,6 +29,11 @@ const routes = [
     path: '/canceled',
     name: 'tc',
     component: CanceledAppointment
+  },
+  {
+    path: '/done',
+    name: 'tc',
+    component: DoneAppoinment
   }
 ]
 
